3d5d331b1f8a74485638db928965c13e52a6a1c8
Max Szymański <maksymilianszymanski2@gmail.com>
2024-07-25 10:57:43 +0200

    save

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,13 +4,10 @@ import Header from '../features/mainPage/Header'
 import HomeProjectsSection from '../features/mainPage/HomeProjectsSection'
 import HomeSkillsSection from '../features/mainPage/HomeSkillsSection'
 import Main from '../ui/Main'
-import { useTranslation } from 'react-i18next'
 
 function MainPage() {
-    const { t } = useTranslation()
     return (
         <>
-            
             <Header />
             <Main $mainPage>
                 <HomeProjectsSection />

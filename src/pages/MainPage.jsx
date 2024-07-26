9eacb5efb08e7d96c9d8c4f61b7a4a9f04c51f92
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2024-07-26 21:07:12 +0200

    change metadata

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,10 +4,18 @@ import Header from '../features/mainPage/Header'
 import HomeProjectsSection from '../features/mainPage/HomeProjectsSection'
 import HomeSkillsSection from '../features/mainPage/HomeSkillsSection'
 import Main from '../ui/Main'
+import { Helmet } from 'react-helmet-async'
 
 function MainPage() {
     return (
         <>
+            <Helmet>
+                <title>Max Szymanski | Frontend Developer</title>
+                <meta
+                    property="og:title"
+                    content="Max Szymanski | Frontend Developer"
+                />
+            </Helmet>
             <Header />
             <Main $mainPage>
                 <HomeProjectsSection />
